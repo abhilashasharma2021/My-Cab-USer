2c53944cfab95a8aa201936bf11a0b1ac48b4d89
abhilashasharma2021 <suhanashuri584@gmail.com>
2021-07-24 13:57:16 +0530

    My Cab User (24th July)

---
+++ app/src/main/java/com/mycabuser/APIData/API.java
@@ -0,0 +1,10 @@
+package com.mycabuser.APIData;
+
+public class API {
+
+    public static final String BASE_URL = "https://developerdeepika.com/mycab/appservice/process.php/";
+
+    public static final String showVehicle="?action=driver_near_me";
+    public static final String ApplyPromo="?action=check_promo_code";
+    public static final String AddRequestBooking="?action=add_request_booking";
+}
